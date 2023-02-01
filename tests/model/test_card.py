b9dc0f1226bc5ffad5d54bcb1f8a159a2fabf2f1
Gennadiy <renardes@gmail.com>
2023-02-01 20:19:31 +0500

    игровые элементы: конфигурация токена, тест множеств
    >class TestToken
      >def test_is_int_within_range
      +def test_set_of_tokens
      >def test_str_straight
      >def test_str_strike

--- tests/model/test_card.py
+++ tests/model/test_card.py
@@ -4,36 +4,35 @@ from loto.model.card import Token
 
 
 class TestToken:
-    minimum = 1
-    maximum = 90
+    Token._minimum = 1
+    Token._maximum = 90
 
     @mark.parametrize('number', [0, 1, -2, 34, 500])
     def test_is_int_within_range(self, number):
-        Token._minimum = self.minimum
-        Token._maximum = self.maximum
-        obj: Token = Token(number)
+        obj = Token(number)
         assert isinstance(obj, int)
-        if number < self.minimum:
+        if number < obj._minimum:
             assert obj == obj._minimum
-        elif number > self.maximum:
+        elif number > obj._maximum:
             assert obj == obj._maximum
         else:
             assert obj == number
 
+    def test_set_of_tokens(self):
+        result = {Token(1), Token(1)}
+        assert result == {Token(1)}
+
     @mark.parametrize('number', [6, 78])
     def test_str_straight(self, number):
-        Token._minimum = self.minimum
-        Token._maximum = self.maximum
         obj = Token(number)
         width = len(str(obj._maximum))
         expected = f'{number:>{width}}'
         assert obj.__str__() == expected
 
     def test_str_strike(self):
-        Token._minimum = self.minimum
-        Token._maximum = self.maximum
         obj = Token()
         obj.strike = True
-        width = len(str(self.maximum))
+        width = len(str(obj._maximum))
         expected = '-'*width
         assert obj.__str__() == expected
+
