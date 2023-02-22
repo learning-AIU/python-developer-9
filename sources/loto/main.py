42a14c0468c5efd9391aedc7281e72e2c7decb6b
Gennadiy <renardes@gmail.com>
2023-02-22 20:35:24 +0500

    точка входа: инициализация контроллера, главный цикл
    >def main

--- sources/loto/main.py
+++ sources/loto/main.py
@@ -2,21 +2,19 @@
 Игра Лото.
 """
 
+from loto import cli
+from loto import controller
 from loto import utils
-
-
-class Controller:
-    """
-
-    """
-
-    @staticmethod
-    def get_input() -> utils.Answer:
-        """"""
 
 
 def main():
     """Инициализирует и запускает игру."""
+    app = controller.Controller(cli.View())
+    app.view.show_line(utils.APP_TITLE)
+    while True:
+        app.view.show_line(utils.MESSAGES['new_game'], level=3)
+        app.set_game()
+        app.start_game()
 
 
 if __name__ == '__main__':
