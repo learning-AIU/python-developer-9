18b0604ef4a37c919220b5f4fe5d06d5d0fc305c
Gennadiy <renardes@gmail.com>
2023-02-22 20:33:31 +0500

    представление интерфейса командной строки
    +class View
      +def show_line
      +def get_players
      +def get_turn
      +def show_token
      +def show_card
      +def show_bot_action
      +def show_winner
      +def show_tie

---
+++ sources/loto/cli.py
@@ -0,0 +1,72 @@
+"""
+Представление для интерфейса командной строки.
+"""
+
+from shutil import get_terminal_size
+from typing import Literal
+
+from loto import game
+from loto import utils
+
+
+class View:
+    """
+    Ввод и вывод данных в интерфейсе командной строки.
+    """
+    terminal_width = get_terminal_size()[0] - 1
+
+    def show_line(self, text: str, level: Literal[1, 2, 3] = 1):
+        if level == 1:
+            text = text.upper()
+        elif level == 2:
+            text = text.title()
+        elif level == 3:
+            text = text.capitalize()
+        filler = getattr(utils, f'FILLER_{level}')
+        line = f" {text} ".center(self.terminal_width-2, filler)
+        print(f'\n {line} ')
+
+    @staticmethod
+    def get_players() -> str:
+        res = ''
+        while True:
+            prompt = (f"\n{utils.SYS_MARK}"
+                      f"{utils.MESSAGES['get_player']}"
+                      f"{utils.SYS_MARK}"
+                      f"\n{utils.PROMPT}")
+            inp = input(prompt)
+            if inp:
+                res += utils.SEP + inp.lower()
+            else:
+                return res.strip(utils.SEP)
+
+    @staticmethod
+    def get_turn() -> str:
+        prompt = (f"\n{utils.SYS_MARK}"
+                  f"{utils.MESSAGES['action']}"
+                  f"{utils.SYS_MARK}"
+                  f"\n{utils.PROMPT}")
+        return input(prompt).lower()
+
+    def show_token(self, token: int):
+        self.show_line(f'Бочонок: {token}', level=2)
+
+    @staticmethod
+    def show_card(player: game.Player):
+        print(f'\n{player.name:^{player.card.width}}')
+        print(player.card)
+
+    @staticmethod
+    def show_bot_action(bot: game.Bot):
+        print(f'\n{utils.SYS_MARK}'
+              f'{bot.name}'
+              f'{utils.SYS_MARK}'
+              f'\n{utils.PROMPT}'
+              f'{utils.MESSAGES[bot.last_action.value]}')
+
+    def show_winner(self, player: game.Player):
+        self.show_line(f'{player.name} победил!', level=3)
+
+    def show_tie(self):
+        self.show_line('Ничья', level=3)
+
