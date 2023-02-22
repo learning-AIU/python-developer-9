02eccc6f3b8efea821190f09a287513ca31ace3d
Gennadiy <renardes@gmail.com>
2023-02-22 20:29:05 +0500

    новые глобальные переменные и константы

--- sources/loto/utils/data.py
+++ sources/loto/utils/data.py
@@ -4,14 +4,27 @@
 
 __all__ = [
     'GameMode',
-    'SAMPLE_LENGTH',
     'DifficultyLvl',
     'Answer',
+    'SAMPLE_LENGTH',
+    'APP_TITLE',
+    'FILLER_1',
+    'FILLER_2',
+    'FILLER_3',
+    'SYS_MARK',
+    'MESSAGES',
+    'PROMPT',
+    'INPUTS',
+    'SEP',
+    'pat_human',
+    'pat_bot_easy',
+    'pat_bot_hard',
 ]
 
 # импорт из модулей/пакетов стандартной библиотеки
 from decimal import Decimal as dec
 from enum import Enum
+from re import compile
 
 
 class GameMode(Enum):
@@ -21,14 +34,53 @@ class GameMode(Enum):
 
 
 class DifficultyLvl(dec, Enum):
-    EASY = dec('0.9')
+    EASY = dec('0.97')
     HARD = dec('1.0')
+
+
+class Answer(Enum):
+    YES = 'yд1'
+    NO = 'nн0'
 
 
 SAMPLE_LENGTH = 1000
 
 
-class Answer(Enum):
-    YES = 'y'
-    NO = 'n'
+APP_TITLE = 'ЛОТО'
+FILLER_1 = '='
+FILLER_2 = '+'
+FILLER_3 = '_'
+
+SYS_MARK = ' _ '
+MESSAGES = {
+    'get_player': 'введите Игрок или Бот Лёгкий или Бот Сложный или пустую строку',
+    'action': 'зачеркнуть? [д/н]',
+    'new_game': 'Новая игра',
+    Answer.NO.value: 'продолжить',
+    Answer.YES.value: 'зачеркнуть',
+    '': '',
+}
+
+PROMPT = ' > '
+INPUTS = {
+    'human': 'игрок',
+    'bot': 'бот',
+    'easy': 'легкий',
+    'hard': 'сложный',
+    '': '',
+}
+
+SEP = '_'
+
+pat_human = compile(
+    rf"{INPUTS['human'][0]}|{INPUTS['human']}"
+)
+pat_bot_easy = compile(
+    rf"(?:{INPUTS['bot'][0]}|{INPUTS['bot']}) ?"
+    rf"(?:{INPUTS['easy'][0]}|{INPUTS['easy']})"
+)
+pat_bot_hard = compile(
+    rf"(?:{INPUTS['bot'][0]}|{INPUTS['bot']}) ?"
+    rf"(?:{INPUTS['hard'][0]}|{INPUTS['hard']})"
+)
 
